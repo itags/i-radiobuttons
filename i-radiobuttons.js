423997cdbcf49187dee0a14c61d91ab37db28dc5
Marco Asbreuk <marco@itsasbreuk.nl>
2015-02-04 00:49:50 +0100

    intermediate saving

---
+++ i-radiobuttons.js
@@ -0,0 +1,31 @@
+module.exports = function (window) {
+    "use strict";
+
+    require('polyfill/polyfill-base.js');
+
+    var itagCore =  require('itags.core')(window),
+        itagName = 'i-radiobuttons', // <-- define your own itag-name here
+        DOCUMENT = window.document,
+        Itag;
+
+    if (!window.ITAGS[itagName]) {
+
+        Itag = DOCUMENT.createItag(itagName, {
+            attrs: {
+            },
+
+            init: function() {
+            },
+
+            sync: function() {
+            },
+
+            destroy: function() {
+            }
+        });
+
+        window.ITAGS[itagName] = Itag;
+    }
+
+    return window.ITAGS[itagName];
+};
